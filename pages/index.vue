b3c8de12f4677314accb15d3f6b7dafff97cb2dd
zhang_jq <1275834973@qq.com>
2024-02-08 11:49:25 +0800

    首页布局

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+</script>
 
 <template>
-  <div class="bg-sky-200">
-    123
-  </div>
+  <Swiper class="h-[40rem] bg-amber-200">
+    <SwiperSlide>Slide 1</SwiperSlide>
+    <SwiperSlide>Slide 2</SwiperSlide>
+    <SwiperSlide>Slide 3</SwiperSlide>
+    <SwiperSlide>Slide 4</SwiperSlide>
+  </Swiper>
 </template>
 
 <style scoped></style>
