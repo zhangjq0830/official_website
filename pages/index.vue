83ee53c056869999e66d821b52e4084db4d440f5
zhang_jq <1275834973@qq.com>
2024-02-09 17:42:53 +0800

    首页布局

--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,11 @@ import 'swiper/css';
 
 <template>
   <!-- 轮播图 -->
-  <Swiper class="h-[40rem] bg-sky-200">
-    <SwiperSlide>Slide 1</SwiperSlide>
+  <Swiper class="h-[50rem] max-w-[100rem] bg-sky-200">
+    <SwiperSlide>
+      <img src="@/assets/images/banner01.jpg" alt="Banner01" />
+    </SwiperSlide>
     <SwiperSlide>Slide 2</SwiperSlide>
-    <SwiperSlide>Slide 3</SwiperSlide>
   </Swiper>
 
   <div>其他内容</div>
