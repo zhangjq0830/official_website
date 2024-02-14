466529ccb8f9070ed1baeba5204ed25c64c17a0a
zhang_jq <1275834973@qq.com>
2024-02-14 11:46:30 +0800

    主营业务

--- pages/index.vue
+++ pages/index.vue
@@ -20,7 +20,7 @@ import 'swiper/css';
     <div>
       <div class="mb-2 text-t-primary">关于我们 / About us</div>
       <div class="mb-4 text-3xl font-bold text-t-primary">亚诺隆德科技有限公司</div>
-      <p class="mb-5 leading-7 tracking-wider text-t-primary text-justify">
+      <p class="mb-5 text-justify leading-7 tracking-wider text-t-primary">
         是一家专注于提供全方位软件解决方案的创新型公司。我们致力于通过尖端的技术和独特的方法，推动客户的业务发展和创新。自成立以来，我们始终坚持以人为本，科技引领的原则，汇聚了一批行业内顶尖的软件开发工程师和技术专家。他们凭借丰富的经验和专业知识，为我们提供了强大的技术支持，使我们能够为客户提供最优质的服务。我们的业务范围涵盖了定制软件开发、移动应用开发、云计算服务、大数据分析等多个领域。无论是初创公司还是大型企业，我们都能提供适合其需求的软件解决方案，帮助他们提升业务效率，实现业务目标。
       </p>
       <button class="button-primary">了解更多</button>
@@ -124,7 +124,50 @@ import 'swiper/css';
     </div>
   </div>
 
+  <!-- 业务咨询 -->
+  <div class="mx-auto flex h-[30rem] max-w-[120rem] justify-end bg-[url('@/assets/images/business.jpg')]">
+    <div class="my-16 w-1/2 text-white">
+      <div class="text-4xl font-bold tracking-wider">
+        <div class="mb-8">来自互联网优秀的技术研发团队</div>
+        <div>随时响应项目需求</div>
+      </div>
+      <div class="mt-12 flex">
+        <div class="mx-8 flex flex-col items-center">
+          <div
+            class="relative mb-1 text-4xl font-bold after:absolute after:-right-5 after:-top-2 after:text-2xl after:content-['+']"
+          >
+            78
+          </div>
+          <div class="text-lg tracking-wider">技术专利</div>
+        </div>
+        <div class="mx-8 flex flex-col items-center">
+          <div
+            class="relative mb-1 text-4xl font-bold after:absolute after:-right-5 after:-top-0 after:text-sm after:content-['年']"
+          >
+            15
+          </div>
+          <div class="text-lg tracking-wider">开发经验</div>
+        </div>
+        <div class="mx-8 flex flex-col items-center">
+          <div
+            class="relative mb-1 text-4xl font-bold after:absolute after:-right-5 after:-top-2 after:text-2xl after:content-['+']"
+          >
+            30
+          </div>
+          <div class="text-lg tracking-wider">研发团队</div>
+        </div>
+      </div>
+    </div>
+  </div>
+
   <!-- 案例分享 -->
-  <div class="app-container py-16">案例分享</div>
+  <div class="app-container py-16">
+    <div class="mb-4 text-center">
+      <div class="mb-2 text-3xl font-bold text-t-primary">案例分享</div>
+      <div class="text-lg text-t-primary">秉持以人为本、客户至上的经营思想</div>
+    </div>
+
+    <div>1231232</div>
+  </div>
 </template>
 <style scoped></style>
